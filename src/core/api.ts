c1732a2b029646d2cad64bd22e245a30f0466056
Daniela Carrasco <dalbacarrasco@gmail.com>
2026-03-23 16:23:34 -0400

    chore: setup structure, axios client and provider

---
+++ src/core/api.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message =
+      error.response?.data?.message || "Ocurrió un error inesperado";
+    return Promise.reject(new Error(message));
+  }
+);
+
+export default api;
